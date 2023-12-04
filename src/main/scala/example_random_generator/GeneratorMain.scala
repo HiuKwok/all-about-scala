865dab49ea17c47c9e44d5c7e5778ba2c4e140ff
HiuFung Kwok <hiufkwok@gmail.com>
2023-12-04 23:22:43 +0800

    Update import

--- src/main/scala/example_random_generator/GeneratorMain.scala
+++ src/main/scala/example_random_generator/GeneratorMain.scala
@@ -1,8 +1,10 @@
 package example_random_generator
 
+import scala.util.Random
+
 object GeneratorMain extends App {
 
-  val rand = new scala.util.Random
+  val rand = new Random
 
   println("Hello: " + rand.nextInt())
 
