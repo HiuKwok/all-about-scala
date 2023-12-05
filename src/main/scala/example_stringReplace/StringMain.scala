b0aec600c02b16b14f307cd64338ca2f87b965c7
HiuFung Kwok <hiufkwok@gmail.com>
2023-12-05 22:49:06 +0800

    Add String example

---
+++ src/main/scala/example_stringReplace/StringMain.scala
@@ -0,0 +1,18 @@
+package example_stringReplace
+
+object StringMain {
+  def main(args: Array[String]) = {
+
+    val name = "Andy"
+    val h = "Hello $name"
+    val count = 1
+
+    // Won't work.
+    println(h)
+    // But this work.
+    println(s"Hello $name")
+    // Can also do arithmetic
+    println(s"1 + 1: ${count + 1}")
+
+  }
+}
