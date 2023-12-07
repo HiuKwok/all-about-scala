95de375d47c705eae7afad6ad7e2868f5445e8b2
HiuFung Kwok <hiufkwok@gmail.com>
2023-12-07 12:18:34 +0800

    Syntax: Inheritance

---
+++ src/main/scala/syntax/inhert/multiLevelInheritance.scala
@@ -0,0 +1,30 @@
+package syntax.inhert
+
+import syntax.traits
+
+/**
+ * A -> B -> C
+ */
+object multiLevelInheritance {
+
+    class A() {
+      val a = "A"
+    }
+
+    class B() extends A() {
+      val b = "B"
+    }
+
+    class C() extends B() {
+      val c = "C"
+    }
+
+    def main(args: Array[String]): Unit = {
+      val c = new C
+      println(s"c.a = ${c.a}") // A property (non direct superclass)
+      println(s"c.b = ${c.b}") // B property (direct superclass)
+      println(s"c.c = ${c.c}") // C property
+    }
+  }
+
+}
---
+++ src/main/scala/syntax/inhert/package.scala
@@ -0,0 +1,5 @@
+package syntax
+
+package object inhert {
+
+}
